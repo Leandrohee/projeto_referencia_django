4c322b8fac23ec9fa2c34e1c8a79712143a15745
unknown <cemev@CEMEV_SELOG13.cbmdf.gov.br>
2024-03-18 18:21:35 -0300

    add opcao buscar

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@
 
 from django.urls import path
 from .views import renderizandoUmArquivoHtml,renderizandoPedidosIndividuais
-from .views import renderizandoSimplesHtml, enviandoDadosSemDB 
+from .views import renderizandoSimplesHtml, enviandoDadosSemDB, renderizandoPaginaBuscar 
 
 urlpatterns = [
     path('htmlsimples/', renderizandoSimplesHtml),                                              #nessa url relativa sera renderizado um html simples 
     path('enviandodadossemdb/',enviandoDadosSemDB),                                             #essa url eh para exibir um envio de dados sem a necessidade de banco de dados 
     path('',renderizandoUmArquivoHtml,name='principal'),                                        #path para o html principal
-    path('pedido/',renderizandoPedidosIndividuais, name='pedido')                               #path para o html de um pedido especifico, esse name eh importante
+    path('pedido/',renderizandoPedidosIndividuais, name='pedido'),                              #path para o html de um pedido especifico, esse name eh importante
+    path('buscar/',renderizandoPaginaBuscar, name='buscar')                                     #path para o html da pagina de buscas
 ]
--- api/views.py
+++ api/views.py
@@ -9,11 +9,21 @@ def renderizandoSimplesHtml(request):                                           
 
 def renderizandoUmArquivoHtml(request):                                                     #fiz uma funcao que ira renderizar um arquivo .html ***EH O PRINCIPAL DO PROJETO***
     pedidos_do_db = Pedido.objects.all()                                                    #pegando todos os dados do modelo Pedido e envia paraa a variavel pedidos_do_db
-    pedidos_filtrados = Pedido.objects.order_by("pedido")                                   #filtrando os pedidos do banco de dados por numero do pedido
-    return render(request, 'principal/index.html',{"pedidos_enviados": pedidos_filtrados})  #para ele conseguer ler o arquivo index.html o caminho da pasta dele deve ser configurada no settings.py na aba DIRS
+    pedidos_organizados = Pedido.objects.order_by("pedido")                                 #filtrando os pedidos do banco de dados por numero do pedido
+    return render(request, 'principal/index.html',{"pedidos_enviados": pedidos_organizados})    #para ele conseguer ler o arquivo index.html o caminho da pasta dele deve ser configurada no settings.py na aba DIRS
 
 def renderizandoPedidosIndividuais(request):                                                #renderiza o html referente a um pedido
     return render(request,'principal/pedido.html')
+
+def renderizandoPaginaBuscar(request):                                                      #criei uma funcao que mostrara itens filtrados
+    pedidos_organizados = Pedido.objects.order_by("pedido")                                 #filtrando os pedidos do banco de dados por numero do pedido
+
+    if "input-buscar" in request.GET:                                                           #se a string "input-buscar" estiver na url (requst.get puxa a url) prossiga
+        pedido_a_buscar = request.GET['input-buscar']                                           #coloca o que vem depois da string 'input-buscar' na url na variavel pedido_a_buscar
+        if pedido_a_buscar:                                                                     #se a variavel pedido_a_buscar nao estiver vazia, ou seja o campo buscar em branco prosseguir
+            pedidos_filtrados = pedidos_organizados.filter(pedido__icontains=pedido_a_buscar)   #__icontains eh uma funcao do django que procura o texto desejado no campo pedido em todos os elementos do modelo Pedido 
+
+    return render(request, 'principal/buscar.html', {"pedidos_enviados": pedidos_filtrados})    #renderiza a pagina buscar.html e envia para ela a lista dos pedidos filtrados pelo numero do pedido    
 
 def enviandoDadosSemDB(request):                                                            #essa funcao de renderizao eh somente para simular o envio de dados sem a ncessidade de um banco de dados
     dados={                                                                                 #criei um dicionario contendo 3 dicionarios dentro para fazer o  envio  dos dados para o html
